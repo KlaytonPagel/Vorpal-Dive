dff4dd63d69068b60e1afccf1f2f792639295edc
Klayton Pagel <pagelk@lopers.unk.edu>
2023-12-20 20:10:26 -0600

    added item IDs
    added a JSON file that stores the items IDs, weapon types, and damage

--- main/code/inventory.py
+++ main/code/inventory.py
@@ -1,26 +1,33 @@
+import json
+
 import pygame
 from sprites import HUD_object
 
 
 class Inventory:
     def __init__(self):
-        self.inventory_slots = {'1': ['occupied', (284, 54), 1], '2': ['occupied', (353, 54), 1],
-                                '3': ['occupied', (422, 54), 1], '4': ['occupied', (491, 54), 1],
-                                '5': ['occupied', (560, 54), 1], '6': ['occupied', (284, 123), 1],
-                                '7': ['occupied', (353, 123), 1], '8': ['occupied', (422, 123), 1],
-                                '9': ['occupied', (491, 123), 1], '10': ['occupied', (560, 123), 1],
-                                '11': ['occupied', (284, 192), 1], '12': ['occupied', (353, 192), 1],
-                                '13': ['occupied', (422, 192), 1], '14': ['occupied', (491, 192), 1],
-                                '15': ['occupied', (560, 192), 1], '16': ['occupied', (284, 261), 1],
-                                '17': ['occupied', (353, 261), 1], '18': ['occupied', (422, 261), 1],
-                                '19': ['occupied', (491, 261), 1], '20': ['occupied', (560, 261), 1],
-                                '21': ['occupied', (284, 330), 1], '22': ['occupied', (353, 330), 1],
-                                '23': ['occupied', (422, 330), 1], '24': ['occupied', (491, 330), 1],
-                                '25': ['occupied', (560, 330), 1], 'equipped': ['occupied', (87, 330), 1]
+        self.inventory_slots = {'1': ['occupied', (284, 54), '1'], '2': ['occupied', (353, 54), '2'],
+                                '3': ['occupied', (422, 54), '1'], '4': ['occupied', (491, 54), '1'],
+                                '5': ['occupied', (560, 54), '2'], '6': ['occupied', (284, 123), '1'],
+                                '7': ['occupied', (353, 123), '1'], '8': ['occupied', (422, 123), '1'],
+                                '9': ['occupied', (491, 123), '1'], '10': ['occupied', (560, 123), '2'],
+                                '11': ['occupied', (284, 192), '1'], '12': ['occupied', (353, 192), '1'],
+                                '13': ['occupied', (422, 192), '1'], '14': ['occupied', (491, 192), '1'],
+                                '15': ['occupied', (560, 192), '1'], '16': ['occupied', (284, 261), '1'],
+                                '17': ['occupied', (353, 261), '1'], '18': ['occupied', (422, 261), '1'],
+                                '19': ['occupied', (491, 261), '1'], '20': ['occupied', (560, 261), '1'],
+                                '21': ['occupied', (284, 330), '1'], '22': ['occupied', (353, 330), '1'],
+                                '23': ['occupied', (422, 330), '1'], '24': ['occupied', (491, 330), '1'],
+                                '25': ['occupied', (560, 330), '1'], 'equipped': ['occupied', (87, 330), '2']
                                 }
         self.inventory_items = []
         self.inventory_group = pygame.sprite.Group()
         self.inventory_menu = None
+        self.item_IDs = self.get_item_IDs()
+
+    def get_item_IDs(self):
+        with open('../json/item_IDs.json') as item_ID_file:
+            return json.load(item_ID_file)
 
     def load_inventory(self):
         # the inventory in its open state
@@ -32,7 +39,7 @@ class Inventory:
         # load each occupied slot in the inventory
         for slot in self.inventory_slots:
             if self.inventory_slots[slot][0] == 'occupied':
-                item = pygame.image.load('../textures/32X32/HUD/wooden_sword_hud.png').convert()
+                item = pygame.image.load(self.item_IDs[self.inventory_slots[slot][2]][0]).convert()
                 item = pygame.transform.scale(item, (64, 64))
                 item = HUD_object(self.inventory_slots[slot][1], item, self.inventory_group)
                 self.inventory_items.append(item)
