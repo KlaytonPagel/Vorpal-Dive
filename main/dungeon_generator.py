5a027042f6eba2b32b35e350e5922e9c07863c21
Klayton Pagel <pagelk@lopers.unk.edu>
2023-12-22 19:03:02 -0600

    reformatted repository
    used pygbag to package into an html and apk file to host on website .reformatted the repository to support pygbag. changed to not save dungeon as an image and to use it as a surface diretly from code

---
+++ main/dungeon_generator.py
@@ -0,0 +1,132 @@
+import random
+import pygame
+from sprites import Tile
+from config import *
+
+
+# Dungeon class for generating a random dungeon____________________________________________________
+class Dungeon:
+    def __init__(self, visible_group, obstacle_group):
+        self.floor_tile_positions = []
+        self.visible_group = visible_group
+        self.obstacle_group = obstacle_group
+        self.player_start_position = None
+        self.dungeon_level = []
+        self.generate_rooms()
+
+    # Choose random places within dungeon size and build rooms_____________________________________
+    def generate_rooms(self):
+        end_position = None
+        for attempt in range(room_attempts):
+
+            # Pick a random point to create a room
+            room_x = random.randint(0, dungeon_width - (max_room_width * tile_size))
+            room_y = random.randint(0, dungeon_height - (max_room_height * tile_size))
+
+            # align the room to a grid of tiles based on the tile size
+            if room_x % tile_size != 0:
+                room_x -= room_x % tile_size
+            if room_y % tile_size != 0:
+                room_y -= room_y % tile_size
+
+            # randomize the room height and width based off of the defined max and min height and width
+            room_width = random.randint(min_room_width, max_room_width)
+            room_height = random.randint(min_room_height, max_room_height)
+
+            # Check if a room will overlap
+            if self.validate_room(room_x, room_y, room_width, room_height):
+
+                # Build out the room from the starting position
+                for h in range(room_height):
+                    for w in range(room_width):
+                        self.floor_tile_positions.append((room_x + w * tile_size, room_y + h * tile_size))
+
+                # if this is not the first room, create a tunnel to the previous room
+                start_position = (room_x + room_width // 2 * tile_size, room_y + room_height // 2 * tile_size)
+                if end_position is not None:
+                    self.create_tunnel(start_position, end_position)
+                else: self.player_start_position = start_position
+                end_position = (room_x + room_width // 2 * tile_size, room_y + room_height // 2 * tile_size)
+
+        # Place wall and floor images
+        self.build_floors()
+        self.build_walls()
+
+    # Checks if there are already tiles in any of the spots the room will occupy___________________
+    def validate_room(self, room_x, room_y, room_width, room_height):
+        for h in range(1, room_height):
+            for w in range(1, room_width):
+                if (room_x + w * tile_size, room_y + h * tile_size) in self.floor_tile_positions:
+                    return False
+        return True
+
+    # Create tunnel from the current room to the previously made room______________________________
+    def create_tunnel(self, start_position, end_position):
+        start_x, start_y = start_position
+        end_x, end_y = end_position
+
+        # travel from the start to the end point on the x-axis
+        while start_x != end_x:
+            if start_x < end_x:
+                start_x += tile_size
+            else:
+                start_x -= tile_size
+
+            # place floor tiles for the tunnel at the defined width
+            start_y -= tile_size
+            for i in range(tunnel_width):
+                if (start_x, start_y) not in self.floor_tile_positions:
+                    self.floor_tile_positions.append((start_x, start_y))
+                start_y += tile_size
+            start_y -= tile_size * (tunnel_width - 1)
+
+        # travel from the start to the end point on the y-axis
+        while start_y != end_y:
+            if start_y < end_y:
+                start_y += tile_size
+            else:
+                start_y -= tile_size
+
+            # place floor tiles for the tunnel at the defined width
+            start_x -= tile_size
+            for i in range(tunnel_width):
+                if (start_x, start_y) not in self.floor_tile_positions:
+                    self.floor_tile_positions.append((start_x, start_y))
+                start_x += tile_size
+            start_x -= tile_size * (tunnel_width - 1)
+
+    # combine all floor tiles into one image and save it___________________________________________
+    def build_floors(self):
+        # Create a new surface for all floor tiles
+        complete_floor_surface = pygame.Surface((dungeon_width, dungeon_height))
+
+        # load all floor tile images and combine all tiles onto one surface
+        floor_tile_image = pygame.image.load('textures/32X32/Floors/Floor 2.png')
+        floor_tile_image = pygame.transform.scale(floor_tile_image, (tile_size, tile_size))
+        for tile in self.floor_tile_positions:
+            complete_floor_surface.blit(floor_tile_image, (tile[0], tile[1]))
+        self.dungeon_level.append(complete_floor_surface)
+
+    # Check if tiles adjacent to occupied tiles are also occupied, if not put a wall tile down_____
+    def build_walls(self):
+        for tile in self.floor_tile_positions:
+
+            # Check tile to the right
+            if (tile[0] + tile_size, tile[1]) not in self.floor_tile_positions:
+                Tile((tile[0] + tile_size, tile[1]),
+                        'textures/32X32/Walls/Wall front.png', (self.visible_group, self.obstacle_group))
+
+            # check tile to the left
+            if (tile[0] - tile_size, tile[1]) not in self.floor_tile_positions:
+                Tile((tile[0] - tile_size, tile[1]),
+                        'textures/32X32/Walls/Wall front.png', (self.visible_group, self.obstacle_group))
+
+            # check tile above
+            if (tile[0], tile[1] + tile_size) not in self.floor_tile_positions:
+                Tile((tile[0], tile[1] + tile_size),
+                        'textures/32X32/Walls/Wall front.png', (self.visible_group, self.obstacle_group))
+
+            # check tile below
+            if (tile[0], tile[1] - tile_size) not in self.floor_tile_positions:
+                Tile((tile[0], tile[1] - tile_size),
+                        'textures/32X32/Walls/Wall front.png', (self.visible_group, self.obstacle_group))
---
+++ main/main.py
@@ -0,0 +1,85 @@
+import pygame
+import asyncio
+from pygame.locals import *
+from config import *
+from dungeon_generator import Dungeon
+from player import Player
+from enemy_spawner import EnemySpawner
+from sprites import Text
+
+# start pygame_____________________________________________________________________________________
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height), RESIZABLE)
+pygame.display.set_caption('py dungeon')
+running = True
+clock = pygame.time.Clock()
+
+
+# Custom Camera Group Setup________________________________________________________________________
+class VisibleGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2(0, 0)
+        self.ground_image = None
+
+    # Center the camera on the player
+    def center_camera(self, target):
+        self.offset.x = target.rect.centerx - pygame.display.get_surface().get_width() // 2
+        self.offset.y = target.rect.centery - pygame.display.get_surface().get_height() // 2
+
+    # Draw all sprites on the screen
+    def custom_draw(self):
+        global dungeon
+        self.center_camera(player)
+
+        # Display Ground before other items
+        ground_offset = (0, 7) - self.offset
+        screen.blit(dungeon.dungeon_level[0], ground_offset)
+
+        # Display items in relation to players character
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            screen.blit(sprite.image, offset_pos)
+
+
+# Declare groups___________________________________________________________________________________
+visible_group = VisibleGroup()
+obstacle_group = pygame.sprite.Group()
+weapon_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+hud_group = pygame.sprite.Group()
+
+# create objects___________________________________________________________________________________
+dungeon = Dungeon(visible_group, obstacle_group)
+player = Player(visible_group, dungeon.player_start_position, visible_group,
+                obstacle_group, weapon_group, enemy_group, hud_group)
+enemy_spawner = EnemySpawner(dungeon.floor_tile_positions, visible_group, enemy_group, obstacle_group, weapon_group)
+
+debug = Text((screen_width - 72, screen_height - 72), str(round(clock.get_fps())), hud_group, 50, (255, 255, 255))
+
+
+async def main():
+    global running
+    while running:
+        event_list = pygame.event.get()
+        for event in event_list:
+            if event.type == pygame.QUIT:
+                player.save_player_data()
+                player.inventory.save_inventory()
+                running = False
+
+        screen.fill((0, 0, 0))
+        visible_group.custom_draw()
+        player.update(event_list)
+
+        enemy_spawner.decide_spawn_enemy(player.rect.center)
+        enemy_group.update(player.rect.center)
+        debug.image = pygame.font.Font(None, 50).render((str(round(clock.get_fps()))), True, (255, 255, 255))
+
+        hud_group.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(fps)
+        await asyncio.sleep(0)
+
+asyncio.run(main())
